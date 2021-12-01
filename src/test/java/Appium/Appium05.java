2c93098519f728811c68b241d8687082c7c21c35
Orcun FAZLI <30703148+ofazli@users.noreply.github.com>
2021-12-01 16:14:10 -0500

    appium

--- src/test/java/Appium/Appium05.java
+++ src/test/java/Appium/Appium05.java
@@ -1,4 +1,69 @@
 package Appium;
 
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.remote.MobileCapabilityType;
+import io.cucumber.java.en.And;
+import org.junit.Assert;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class Appium05 {
+    @Test
+
+    public void test() throws MalformedURLException, InterruptedException {
+        /*
+
+          install app
+          do any mathematical operation
+          and verify pre result and final result
+
+         */
+        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+        desiredCapabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2");
+        desiredCapabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
+        desiredCapabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "AndroidEmulator");
+        desiredCapabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "8.0");
+        desiredCapabilities.setCapability(MobileCapabilityType.APP, "/Users/orcunfazli/Downloads/mobileTest/Apps/Calculator.apk");
+        desiredCapabilities.setCapability(MobileCapabilityType.NO_RESET, true);
+        AndroidDriver<AndroidElement> driver = new AndroidDriver<AndroidElement>(new URL("http://0.0.0.0:4723/wd/hub"), desiredCapabilities);
+
+        AndroidElement num0 = driver.findElementById("com.google.android.calculator:id/digit_0");
+        AndroidElement num1 = driver.findElementById("com.google.android.calculator:id/digit_1");
+        AndroidElement num2 = driver.findElementById("com.google.android.calculator:id/digit_2");
+        AndroidElement num3 = driver.findElementById("com.google.android.calculator:id/digit_3");
+        AndroidElement num4 = driver.findElementById("com.google.android.calculator:id/digit_4");
+        AndroidElement num5 = driver.findElementById("com.google.android.calculator:id/digit_5");
+        AndroidElement num6 = driver.findElementById("com.google.android.calculator:id/digit_6");
+        AndroidElement num7 = driver.findElementById("com.google.android.calculator:id/digit_7");
+        AndroidElement num8 = driver.findElementById("com.google.android.calculator:id/digit_8");
+        AndroidElement num9 = driver.findElementById("com.google.android.calculator:id/digit_9");
+        AndroidElement del = driver.findElementById("com.google.android.calculator:id/del");
+        AndroidElement des = driver.findElementById("com.google.android.calculator:id/op_div");
+        AndroidElement mul = driver.findElementById("com.google.android.calculator:id/op_mul");
+        AndroidElement sub = driver.findElementById("com.google.android.calculator:id/op_sub");
+        AndroidElement dd = driver.findElementById("com.google.android.calculator:id/op_add");
+        AndroidElement equ = driver.findElementById("com.google.android.calculator:id/eq");
+
+        num1.click();
+        num5.click();
+        mul.click();
+        num3.click();
+        Thread.sleep(1500);
+        String actual = driver.findElementById("com.google.android.calculator:id/result_preview").getText();
+        Assert.assertEquals(actual, "45");
+
+        Thread.sleep(3000);
+
+        equ.click();
+        driver.closeApp();
+
+
+    }
+
 }
+
+
---
+++ src/test/java/Appium/Appium06Chrome.java
@@ -0,0 +1,52 @@
+package Appium;
+
+import com.sun.tools.javac.comp.Enter;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.remote.MobileCapabilityType;
+import org.junit.Assert;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Set;
+
+public class Appium06Chrome {
+    @Test
+
+    public void test() throws MalformedURLException, InterruptedException {
+        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+        desiredCapabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2");
+        desiredCapabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
+        desiredCapabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "Emulator");
+        desiredCapabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "11.0");
+        desiredCapabilities.setCapability(MobileCapabilityType.BROWSER_NAME, "chrome");
+
+        desiredCapabilities.setCapability(MobileCapabilityType.NO_RESET, true);
+        AndroidDriver<AndroidElement> driver = new AndroidDriver<AndroidElement>(new URL("http://0.0.0.0:4723/wd/hub"), desiredCapabilities);
+        driver.get("https://www.amazon.com");
+        driver.findElementByXPath("//*[@id=\"nav-logobar-greeting\"]").click();
+        String actuaTitle = driver.findElementByXPath("//*[@id=\"outer-accordion-signin-signup-page\"]/h2").getText();
+        Thread.sleep(2000);
+        String extual = "Welcome";
+        Thread.sleep(1000);
+        Assert.assertEquals(actuaTitle,extual);
+        Set contextNames = driver.getContextHandles();
+
+        for (Object contextName : contextNames) {
+            System.out.println(contextName);//NATIVE_APP   CHROMIUM
+            Thread.sleep(3000);
+            if (contextName.toString().contains("CHROMIUM")){
+                driver.context((String) contextName);//WEBAPP DEVAM EDECEGIM
+                Thread.sleep(10000);
+            }
+        }
+
+
+        driver.closeApp();
+
+
+    }
+
+}
